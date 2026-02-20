98c6901393d523cb29d5e4ec074bda520add3f17
salim <salim.belkacemm@protonmail.com>
2026-02-20 18:04:29 +0100

    global macros and typedefs forgot to commit it

--- src/def.h
+++ src/def.h
@@ -1,3 +1,21 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 1280
+
+typedef uintptr_t uptr;
+
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
+
+typedef struct {u8 x; u8 y;} vec2;
+
 typedef struct {
   char* str;
   int size;
