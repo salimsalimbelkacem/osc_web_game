5087b7840443d1e8c3bb0f15899ed5483747aa11
salim <salim.belkacemm@protonmail.com>
2026-02-21 07:18:53 +0100

    RELEASE ME

--- src/core/include/ui.h
+++ src/core/include/ui.h
@@ -1,6 +1,26 @@
 #include <raylib.h>
 #include "arena.h"
 
+//default ui styles
+const u32 UI_DEF_FG_COLOR     = 0xc1c1c1;
+
+const u32 UI_DEF_BG_COLOR     = 0x1e1e1e;
+const u32 UI_DEF_BORDER_COLOR = 0xe1e1e1;
+
+// text and heading;
+const string UI_DEF_TXT_FONT     = STR("sans");
+const u16 UI_DEF_TXT_FONT_SIZE   = 12;
+
+const string UI_DEF_HEADING_FONT = STR("sans");
+const u16 UI_DEF_H1_FONT_SIZE    = 24;
+const u16 UI_DEF_H2_FONT_SIZE    = 20;
+const u16 UI_DEF_H3_FONT_SIZE    = 16;
+
+// buttons;
+const i16 UI_DEF_BUTTON_WIDTH    = 50;
+const i16 UI_DEF_BUTTON_HEIGHT   = 25;
+
+
 typedef enum {
     UI_STATE_NONE = 0,
     UI_STATE_HOVERED = 1 << 0,
@@ -18,6 +38,7 @@ typedef enum { UI_CONTENT_ALIGN_TOP, UI_CONTENT_ALIGN_MIDDLE, UI_CONTENT_ALIGN_B
 typedef void (*UI_Action)(struct UI_element*);
 
 typedef struct UI_element {
+  string id;
   struct Rectangle area;
 
   UI_Action onClick_left;
@@ -29,6 +50,7 @@ typedef struct UI_element {
   u16 layer;
 
   struct UI_element* next;
+  struct UI_element* parent;
 
   struct UI_Background{
     UI_Bg_Type type;
@@ -49,44 +71,46 @@ typedef struct UI_element {
     UI_Content_Type type;
     UI_Content_HAlign_Type h_align;
     UI_Content_VAlign_Type v_align;
+    u16 padding;
 
-    u16 padding;    
-    u16 children_gap;    
-    bool stack_content;    
+    struct {
+      string content;
+      u8 font_size; // add font?
+    } text;
 
-    union {
-      struct {
-        string content; u8 font_size; // add font?
-        } text;
-
-      struct Image Image;
-      struct {
-        UI_element *children;
-	u8 count;
-        } children;
-      };
-
+    struct Image icon;
+    
+    u8 children_gap;
+    u8 children_count;
+    bool stack_content;
+    struct UI_element *children;
   } content;
+
 } UI_element;
 
 typedef struct {
-  arena a;
+  arena* a;
   UI_element* root;
 } UI_root;
 
-UI_root UI_Init();
-UI_element* UI_InitElement( vec2 coord, arena* a);
+UI_root UI_Init(arena* a);
+UI_element* UI_InitElement( arena* a);
 
 void UI_AppendElement(UI_element* e, UI_element* new_e);
 void UI_PopElement(UI_element* e, UI_element* new_e);
-void UI_AppendChild(UI_element* parent);
+void UI_AppendChild(UI_element* parent, UI_element* new_e);
 void UI_PopChild(UI_element* parent); // should pop all sub-children obviously
 
 
-UI_element* UI_Button(string text);
-UI_element* UI_Textbox(string text);
-UI_element* UI_Icon(struct Image icon);
-UI_element* UI_InputBox(string placeholder); // do i need this?
+UI_element* UI_Text_Button(arena* a, string text);
+UI_element* UI_Icon_Button(arena* a, struct Image icon);
+
+UI_element* UI_Textbox(arena* a, string text);
+UI_element* UI_Heading(arena* a, string text, u8 level);
+UI_element* UI_Icon(arena* a, struct Image icon);
+UI_element* UI_InputBox(arena* a, string placeholder); // do i need this?
 
 void UI_ElementSetState();
-void UI_ElementUnsetState();
+void UI_ElementunSetState();
+
+void UI_ElementMapInput(UI_element);
--- src/core/ui.c
+++ src/core/ui.c
@@ -6,23 +6,35 @@ static inline UI_element* UI_Alloc(arena* a) {
   return (UI_element*) ARENA_Alloc( a, sizeof(UI_element));
 }
 
-UI_root UI_Init(){
-  arena a = ARENA_Init();
-  UI_element* r = UI_InitElement((vec2){0, 0}, &a);
+static inline UI_element* UI_ListGetTail(UI_element* e) {
+  if(!e) return NULL;
+
+  UI_element* p = e;
+  for(; p->next != NULL; p=p->next){}
+
+  return p;
+}
+
+UI_root UI_Init(arena* a){
+  UI_element* r = UI_InitElement(a);
 
   r->area.width = SCREEN_WIDTH;
   r->area.height = SCREEN_HEIGHT;
 
+  r->id = STR("root");
+
   return (UI_root) {a, r};
 }
 
-UI_element* UI_InitElement( vec2 coord, arena* a){
+UI_element* UI_InitElement( arena* a){
   UI_element* e = UI_Alloc(a);
+  e->id = STR(NULL);
 
-  e->area.x = coord.x;
-  e->area.y = coord.y;
+  e->area.x = 0;
+  e->area.y = 0;
   e->area.width = 0;
   e->area.height = 0;
+  e->content.text.font_size = 12;
 
   e->onClick_left = NULL;
   e->onClick_right = NULL;
@@ -45,19 +57,99 @@ UI_element* UI_InitElement( vec2 coord, arena* a){
     .padding = 0,
     .children_gap = 0,
     .stack_content = false,
+    .text.font_size = UI_DEF_TXT_FONT_SIZE,
   };
 
   return e;  
 }
 
+void inline UI_AppendElement(UI_element *e, UI_element *new_e) {
+  UI_element *tail = UI_ListGetTail(e);
 
-void UI_AppendElement(UI_element* e, UI_element* new_e){}
-void UI_PopElement(UI_element* e, UI_element* new_e){}
-void UI_AppendChild(UI_element* parent){}
-void UI_PopChild(UI_element* parent){}
+  tail->next = new_e;
+  new_e->next = NULL;
+}
+
+void UI_AppendChild(UI_element* parent, UI_element* new_e){
+
+  new_e->layer = parent->layer;
+
+  if (parent->content.children == NULL) {
+    parent->content.children = new_e;
+    new_e->parent = parent;
+    new_e->next = NULL;
+
+    return;    
+  }
+
+  UI_AppendElement( parent->content.children, new_e);
+  new_e->next = NULL;
+}
+
+/* void UI_PopChild(UI_element* parent){} */
+/* void UI_PopElement(UI_element* e, UI_element* new_e){} */
 
 
-UI_element* UI_Button(string text){}
-UI_element* UI_Textbox(string text){}
-UI_element* UI_Icon(struct Image icon){}
-UI_element* UI_InputBox(string placeholder){}
+UI_element* UI_TextButton(arena* a, string text, UI_Action onclick){
+  UI_element* butt = UI_InitElement(a);
+  butt->id = ARENA_strcpy(a, STR("button"));
+
+  butt->content.type = UI_CONTENT_TEXT;  
+  butt->content.text.content = ARENA_strcpy(a, text);
+
+  return butt;  
+}
+
+UI_element* UI_IconButton(arena* a, struct Image icon){
+  UI_element* butt = UI_InitElement(a);
+  butt->id = ARENA_strcpy(a, STR("button"));
+
+  butt->content.type = UI_CONTENT_IMAGE;  
+  butt->content.icon = icon;
+  
+  return butt;
+}
+
+UI_element* UI_Textbox(arena* a, string text){
+  UI_element* text_box = UI_InitElement(a);
+  text_box->id = ARENA_strcpy(a, STR("button"));
+  
+  text_box->content.type = UI_CONTENT_TEXT;  
+  text_box->content.text.content = ARENA_strcpy(a, text);
+
+  return text_box;  
+}
+
+UI_element *UI_Heading(arena *a, string text, u8 level) {
+  UI_element* heading = UI_InitElement(a);
+
+  heading->content.type = UI_CONTENT_TEXT;
+  heading->content.text.content = ARENA_strcpy(a, text);
+
+  switch (level) {
+  case 1:
+    heading->content.text.font_size = UI_DEF_H1_FONT_SIZE;
+    break;
+
+  case 2:
+    heading->content.text.font_size = UI_DEF_H2_FONT_SIZE;
+    break;
+
+  case 3:
+    heading->content.text.font_size = UI_DEF_H3_FONT_SIZE;
+    break;
+  }
+
+  return heading;  
+}
+
+UI_element* UI_Icon(arena* a, struct Image icon){}
+UI_element* UI_InputBox(arena* a, string placeholder){}
+
+void ui_setElementState(){
+
+}
+
+void ui_UnsetElementState(){
+
+}
